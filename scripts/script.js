f41308373334773fcd1e1fac15f34acf737cc7da
McaddyAdam <ade22001@byui.edu>
2023-01-12 11:28:12 -0800

    WDD230_Assignment_portal

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = `© ${year} .:|:. Adebesin Adam Toyosi .:|:. Nigeria`
+
+
+document.querySelector(".updated").innerHTML = `Last Updated: ${document.lastModified}`;
