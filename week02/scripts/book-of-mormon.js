02ce697691120699e6ce371c2ada5ef5fa639477
Adebesin Adam <ade22001@byui.edu>
2024-05-08 10:55:47 +0100

    BOM

---
+++ week02/scripts/book-of-mormon.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('button');
+    const list = document.querySelector('#list');    
+
+    button.addEventListener('click', function () {
+        if (input.value !== '') {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+
+            li.textContent = input.value;
+            deleteButton.textContent = '❌';
+
+            li.append(deleteButton);
+            list.append(li);
+
+            deleteButton.addEventListener('click', function () {
+                list.removeChild(li);
+                input.focus();
+            });
+
+            input.value = '';
+            input.focus();
+        } else {
+            alert('Please enter a chapter.');
+            input.focus();
+        }
+    });
+});
