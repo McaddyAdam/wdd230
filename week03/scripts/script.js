86d961e80eeffda20c37aef3ddb11a9f4112c859
Adebesin Adam <ade22001@byui.edu>
2024-05-18 16:47:54 +0100

    lazyload

---
+++ week03/scripts/script.js
@@ -0,0 +1,29 @@
+const images = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+
+            }
+        });
+    });
+    images.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+}
